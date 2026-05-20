47d391c3e0d334d37ea2411865b316ecd0844fc7
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-05-19 21:22:43 -0400

    feat(products): agregando nuevo campo al modelo

--- backend/products/models.py
+++ backend/products/models.py
@@ -12,8 +12,15 @@ class Product(models.Model):
         ('no_comestibles', 'No comestibles'),
         ('otros', 'Otros'),
     )
+    TYPE_MACHINE = (
+        ('vending', 'Vending Machine'),
+        ('coffee', 'Coffee Machine'),
+        ('snack', 'Snack Machine'),
+        ('other', 'Other'),
+    )
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=50, choices=TYPE, default='cafe')
+    type_machine = models.CharField(max_length=20, choices=TYPE_MACHINE, default='coffee')
     supplier = models.CharField(max_length=100, blank=True, null=True)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     price = models.DecimalField(max_digits=10, decimal_places=2)
