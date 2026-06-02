2b2ad16f1b8bdefe35c588ffeb12069fc4d282bf
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-06-02 17:41:10 -0400

    feat(supplies): realizando peticion al backend y mostrando productos

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,22 @@
 // Función para poder reutilizar en todas las peticiones de obtención de datos
 export const loadDataFromAPI = async (endpoint, setData) => {
     try {
-      const response = await fetch(`http://localhost:8000/api/${endpoint}/`);
+      const response = await fetch(`http://localhost:8000/api/${endpoint}/`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      
+      // Verificar si la respuesta es OK
+      if (!response.ok) {
+        throw new Error(`Error ${response.status}: ${response.statusText}`);
+      }
+      
       const data = await response.json();
       setData(data);
     } catch (error) {
-      console.error('Error al cargar las plazas:', error);
+      console.error('Error al cargar supplies:', error);
+      // Aquí puedes ver exactamente qué está fallando
     }
 };
